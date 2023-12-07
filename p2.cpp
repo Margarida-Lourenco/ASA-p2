7de6807e500cb98284515b11cd32eb8ea25718c0
Margarida Ascensão Lourenço <margarida.a.lourenco@tecnico.ulisboa.pt>
2023-12-07 08:18:05 +0000

    só para escrever algo

---
+++ p2.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+#include <string>
+
+
+// nao ligar muito para o codigo, so para testar o git mesmo
+
+// Vertex struct
+typedef struct {
+    int id;
+    bool visited;
+    std::vector<int> edges;
+} vertex;
+
+int main(){
+    return 0;
+}
