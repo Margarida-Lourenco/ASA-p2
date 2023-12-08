01b4b5659d4818631848858e064b8b6a0d335fe6
Margarida Ascensão Lourenço <margarida.a.lourenco@tecnico.ulisboa.pt>
2023-12-08 21:22:44 +0000

    input funcional

--- p2.cpp
+++ p2.cpp
@@ -3,9 +3,7 @@
 #include <string>
 #include <iostream>
 #include <string>
-
-
-// nao ligar muito para o codigo, so para testar o git mesmo
+#include <sstream>
 
 // Vertex struct
 typedef struct {
@@ -16,19 +14,32 @@ typedef struct {
 
 int main(){
     int n, m;
-    scanf("%d %d", &n, &m);
+    std::string line;
+    std::vector<vertex> graph;
+    
+    if (!std::getline(std::cin, line) || sscanf(line.c_str(), "%d %d", &n, &m) != 2) {
+        std::cout << "Erro na leitura." << std::endl;
+        return 1;
+    }
 
     if ((n < 2) || m < 0)
         return 1;
 
-    while(true){
-        std::string line;
-        std::getline(std::cin, line);
+    while (std::getline(std::cin, line)) {
+        std::istringstream iss(line);
+        int v1, v2;
 
-        if (line.empty())
+        if (!(iss >> v1 >> v2)) {  // error when reading, stop condition
             break;
+        }
 
-        // arranajr maneira de guardar as relacoes
+        if (v1 >= 1 && v1 <= n && v2 >= 1 && v2 <= n) {
+            graph[v1 - 1].edges.push_back(v2);
+            graph[v2 - 1].edges.push_back(v1);  // n sei se e suposto
+        } else {
+            return 1;   // values out of range
+        }
+
     }
     
     return 0;
