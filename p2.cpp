e8f9b9fd7f02f8835c94b890f0d4517766853edf
Margarida Ascensão Lourenço <margarida.a.lourenco@tecnico.ulisboa.pt>
2023-12-10 11:08:00 +0000

    DFS, transpor o grafo, segunda DFS para obter SCCs

--- p2.cpp
+++ p2.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <stdio.h>
-#include <string>
 #include <iostream>
 #include <string>
 #include <sstream>
@@ -12,35 +11,88 @@ typedef struct {
     std::vector<int> edges;
 } vertex;
 
+
+int firstDFS(std::vector<vertex> &graph, int currentVertex, std::vector<int> &endTimeList){
+    if (graph[currentVertex].visited)
+        return 0;
+    
+    graph[currentVertex].visited = true;
+
+    for (int i = 0; i < (int) graph[currentVertex].edges.size(); i++){
+        firstDFS(graph, graph[currentVertex].edges[i], endTimeList);
+    }
+
+    endTimeList.push_back(currentVertex);   // list of vertices in order of decreasing end time
+    return 0;
+}
+
+
+void transposeGraph(std::vector<vertex> &graph, std::vector<vertex> &transposedGraph){
+    for (int i = 1; i < (int) graph.size(); i++){
+        for (int j = 0; j < (int) graph[i].edges.size(); j++){
+            transposedGraph[graph[i].edges[j]].edges.push_back(i);
+        }
+    }
+}
+
+int secondDFS(std::vector<vertex> &graph, int currentVertex, const std::vector<int> &endTimeList){
+    // reset visited
+    for (auto &v : graph){
+        v.visited = false;
+    }
+
+    for (int i = 0; i < (int) endTimeList.size(); i++){
+        if (graph[endTimeList[i]].visited)
+            continue;
+        
+        graph[endTimeList[i]].visited = true;
+        for (int j = 0; j < (int) graph[endTimeList[i]].edges.size(); j++){
+            secondDFS(graph, graph[endTimeList[i]].edges[j], endTimeList);
+        }
+    }
+    return 0;
+}
+    
 int main(){
     int n, m;
     std::string line;
-    std::vector<vertex> graph;
+    std::vector<int> endTimeList;
     
     if (!std::getline(std::cin, line) || sscanf(line.c_str(), "%d %d", &n, &m) != 2) {
-        std::cout << "Erro na leitura." << std::endl;
         return 1;
     }
 
     if ((n < 2) || m < 0)
         return 1;
 
+    std::vector<vertex>graph (n+1, {0, false, {}});
+
     while (std::getline(std::cin, line)) {
         std::istringstream iss(line);
-        int v1, v2;
+        int x, y;
 
-        if (!(iss >> v1 >> v2)) {  // error when reading, stop condition
+        if (!(iss >> x >> y)) {  // error when reading, stop condition
             break;
         }
 
-        if (v1 >= 1 && v1 <= n && v2 >= 1 && v2 <= n) {
-            graph[v1 - 1].edges.push_back(v2);
-            graph[v2 - 1].edges.push_back(v1);  // n sei se e suposto
+        if (x >= 1 && x <= n && y >= 1 && y <= n) {
+            graph[x].id = x;
+            graph[x].visited = false;
+            graph[x].edges.push_back(y);
         } else {
             return 1;   // values out of range
         }
-
     }
-    
+
+    for (int i = 1; i <= n; i++){
+        firstDFS(graph, i, endTimeList);
+    }
+
+    std::vector<vertex> transposedGraph (n+1, {0, false, {}});
+    transposeGraph(graph, transposedGraph);
+
+    for(int i = 0; i < (int) endTimeList.size(); i++){
+        secondDFS(transposedGraph, 1, endTimeList);
+    }
     return 0;
 }
