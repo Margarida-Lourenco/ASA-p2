19754eaaa980f3ec748dccef6b368c472bd77f8c
Inês Paredes <ines.paredes@tecnico.ulisboa.pt>
2023-12-20 00:16:01 +0000

    Otimização

--- p2ines.cpp
+++ p2ines.cpp
@@ -16,10 +16,9 @@ typedef struct {
 int maxJumps = 0;
 
 /**
- * 1st DFS - finds the vertices ordered by decreasing end time
+ * 1st DFS - finds the end time of each vertex
  * @param graph - graph to be explored
- * @param currentVertex - current vertex
- * @param endTimeStack - Empty stack to be filled with the vertices ordered by decreasing end time
+ * @param endTimeStack - stack to be filled with the vertices in decreasing order of end time
 */
 void firstDFS(vector<vertex> &graph, stack<int> &endTimeStack) {
     for (int start = 1; start < (int) graph.size(); start++) {
@@ -41,7 +40,7 @@ void firstDFS(vector<vertex> &graph, stack<int> &endTimeStack) {
                     }
                 }
             } else {
-                // If the vertex has already been added to the endTimeStack, pop it without pushing again
+                // If the vertex has already been visited, it means that all its neighbors have been visited
                 s.pop();
 
                 if (graph[current].searchtime == false) {
@@ -56,9 +55,8 @@ void firstDFS(vector<vertex> &graph, stack<int> &endTimeStack) {
 /**
  * 2nd DFS - finds the SCCs
  * @param graph - graph to be explored
- * @param currentVertex - current vertex
- * @param scc - Empty set to be filled with the vertices of the SCC
- * @return 1 if the SCC was found, 0 otherwise
+ * @param sccs - vector of SCCs
+ * @param endTimeStack - stack with the vertices in decreasing order of end time
 */
 void secondDFS(vector<vertex> &graph, vector<int> &sccs, stack<int> &endTimeStack) {
     int scc = 1;    
@@ -68,7 +66,7 @@ void secondDFS(vector<vertex> &graph, vector<int> &sccs, stack<int> &endTimeStac
         if (graph[vertex].visited)
             continue;
 
-        if (graph[vertex].edges.empty()) {
+        if (graph[vertex].edges.empty()) { // if the vertex has no edges, it is a SCC
             graph[vertex].visited = true;
             sccs[vertex] = scc;
             scc++;
@@ -98,17 +96,16 @@ void secondDFS(vector<vertex> &graph, vector<int> &sccs, stack<int> &endTimeStac
         scc++;
     }
 }
-/**
- * 3rd DFS - finds the maximum number of jumps
- * @param graph - graph to be explored
- * @param sccs - vector of SCCs
-*/
 
+/**
+ * 3rd DFS - finds the maximum distance from each vertex to a vertex with no outgoing edges
+ * @param graph - graph to be explored
+*/
 void thirdDFS(vector<vertex> &graph) {
     vector<int> distance(graph.size(), 0);
 
     for (int i = 1; i < (int) graph.size(); i++) {
-        if (graph[i].edges.empty())
+        if (graph[i].edges.empty()) 
             continue;
         vector<bool> visited(graph.size(), false);
         stack<int> stack;
@@ -137,12 +134,11 @@ void thirdDFS(vector<vertex> &graph) {
 }
 
 /**
- * Creates the SCC graph
+ * Creates a graph with the SCCs as vertices
  * @param sccs - vector of SCCs
  * @param graph - graph to be explored
- * @return SCC graph
+ * @return a graph with the SCCs as vertices
 */
-
 vector<vertex> createSCCGraph(const vector<int> &sccs, const vector<vertex> &graph) {
     vector<vertex> sccGraph(graph.size(), {false, 0, {}});
 
@@ -157,6 +153,8 @@ vector<vertex> createSCCGraph(const vector<int> &sccs, const vector<vertex> &gra
             stack.pop();
 
             for (int neighbor : graph[current].edges) {
+
+                // If the neighbor is not in the same SCC and the edge has not been added yet
                 if (sccs[current] != sccs[neighbor] && sccGraph[sccs[current]].addedEdges.count(sccs[neighbor]) == 0) {
                     sccGraph[sccs[current]].edges.push_back(sccs[neighbor]);
                     sccGraph[sccs[current]].addedEdges.insert(sccs[neighbor]);
@@ -181,7 +179,7 @@ int main() {
             return 1;
 
         if (x >= 1 && x <= n && y >= 1 && y <= n) {
-            graph[x].edges.push_back(y);
+            graph[x].edges.emplace_back(y);
         } else {
             return 1; // values out of range
         }
@@ -192,7 +190,7 @@ int main() {
 
     firstDFS(graph, endTimeStack);
 
-    vector<vertex> transposedGraph(graph.size(), {false, 0, {}});
+    vector<vertex> transposedGraph(graph.size(), {false, {}});
     for (int i = 1; i < (int) graph.size(); i++) {
         for (int j : graph[i].edges) {
             transposedGraph[j].edges.push_back(i);
@@ -201,14 +199,14 @@ int main() {
 
     secondDFS(transposedGraph, sccs, endTimeStack);
 
-    for (vertex &v : transposedGraph)
-        v.edges.clear();
+    for (auto &v : transposedGraph)
+    v.edges.clear();
 
     transposedGraph.clear();
 
     vector<vertex> sccGraph = createSCCGraph(sccs, graph);
 
-    for (vertex &v : graph)
+    for (auto &v : graph)
         v.edges.clear();
 
     graph.clear();
@@ -219,30 +217,3 @@ int main() {
     return 0;
 }
 
-
-/*
-    ===========================================================================
-    1. Encontrar os SCCs de um grafo
-        * SCC - Strongly Connected Component
-
-        * Este algoritmo dá-nos os SCCs de um grafo, ou seja, os conjuntos de
-         vértices que estão todos ligados entre si, mas não estão ligados a
-         nenhum vértice fora do conjunto.
-        
-        * Nós queremos as ligações entre os SCCs, ou seja, os vértices que
-         ligam SCCs diferentes. Para isso, basta-nos encontrar os vértices
-         que têm arcos para vértices fora do seu SCC.
-    ===========================================================================
-
-    O algoritmo para chegar aos SCCs de um grafo:
-
-    -> Fazer uma DFS normal, guardando uma lista com os vértices ordenada 
-    de modo decrescente pelos respetivos tempos de fim.
-
-    -> Transpor o grafo - alterar o sentido de todos os seus arcos.
-
-    ->Fazer outra DFS (ao grafo transposto), seguindo desta vez a ordem 
-    decrescente que guardámos no primeiro passo. A ordem decrescente é relevante 
-    ao escolher a raiz do caminho, mas aquando da exploração do caminho em si, 
-    não importa - podemos escolher qualquer vértice.
-*/
